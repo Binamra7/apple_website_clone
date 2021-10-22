cdfe75b7092b35f724b4cf5d01d95e32aa75cfb4
Binamra7 <binamra7khadka6@gmail.com>
2021-10-22 20:04:35 +0545

    changes soem formatting

--- src/components/pages/Sidebar.jsx
+++ src/components/pages/Sidebar.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import "./sidebar.css"
+import React from "react";
+import "./sidebar.css";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <div className="list">
-                <ul>
-                    <li>
-                    <a style={{ textDecoration: "none" }} href="sketches">Sketches</a>
-                        </li>
-                    <li>
-                    <a style={{textDecoration: "none"}} href="paintings">Paintings</a>
-                        </li>
-                    <li>
-                    <a style={{textDecoration: "none"}} href="wall_paintings">Wall Paintings</a>
-                        </li>
-                </ul>
-            </div>
-            <div className="settings"></div>
-            
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <div className="list">
+        <ul>
+          <li>
+            <a style={{ textDecoration: "none" }} href="sketches">
+              Sketches
+            </a>
+          </li>
+          <li>
+            <a style={{ textDecoration: "none" }} href="paintings">
+              Paintings
+            </a>
+          </li>
+          <li>
+            <a style={{ textDecoration: "none" }} href="wall_paintings">
+              Wall Paintings
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="settings"></div>
+    </div>
+  );
 }
-
-export default Sidebar
+export default Sidebar;
