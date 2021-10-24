76c7ed4f332efb0fea0b9d8a2092d59ee91fa0d7
Binamra7 <binamra7khadka6@gmail.com>
2021-10-24 20:38:16 +0545

    added footer

---
+++ src/components/pages/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <div className="footer">
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer
