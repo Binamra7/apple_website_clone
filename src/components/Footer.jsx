64b61f3b69f5d8d6fa7b022c413dfa5d1b6588c5
Binamra7 <binamra7khadka6@gmail.com>
2022-05-09 20:57:02 +0545

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import "./Footer.css"
-import footer from "../images/footer.PNG"
+/** @format */
+
+import React from "react";
+import "./Footer.css";
+import footer from "../images/footer.PNG";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <img src={footer} alt="footer info" className="footer__image"/>
-        </div>
-    )
+	const desc = (
+		<p>
+			$4.99/month after free trial. Only one offer per Apple ID and only one
+			offer per family if you’re part of a Family Sharing group, regardless of
+			the number of devices you or your family purchases. This offer is not
+			available if you or your Family have previously accepted an Apple TV+ one
+			year free offer. Offer good for 3 months after eligible device activation.
+			Plan automatically renews until cancelled. Restrictions and other terms
+			apply.
+		</p>
+	);
+
+	return (
+		<div className="footer">
+			<div className="footer__desc">{desc}</div>
+			<hr />
+			<div className="footer__list"></div>
+			<div className="footer__copyright"></div>
+		</div>
+	);
 }
 export default Footer;
